9960b76e84e1d9b6e18adffa7ef9f887d7f56717
pranto <polasahammedpranto@gmail.com>
2022-03-25 21:09:20 +0600

    bike data loaded to Bike shop component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Bikes from './components/Bike Shop/Bikes';
 
 function App() {
   return (
     <div className="App">
-      
+      <Bikes></Bikes>
     </div>
   );
 }
--- src/components/Bike Shop/Bikes.js
+++ src/components/Bike Shop/Bikes.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Bikes = () => {
+    const [bikes, setBikes] = useState([]);
+    useEffect(() => {
+        fetch('BikeData.json')
+        .then(res => res.json())
+        .then(data => setBikes(data))
+    },[])
     return (
         <div>
-            
+            {
+                bikes.map((bike) => console.log(bike.name))
+            }
         </div>
     );
 };
